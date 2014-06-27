9bba05912fc298c063d084c9d52addc143ac5ff2
imperugo <imperugo@gmail.com>
2014-06-27 10:24:38 +0200

    Some work

--- php/tmpl/mail_content.php
+++
@@ -1,6 +0,0 @@
-<p><strong>Name:</strong> <?php echo $vars['name'] ?></p>
-<p><strong>Email:</strong> <?php echo $vars['email'] ?></p>
-<hr />
-<div class="content">
-	<?php echo $vars['message'] ?>
-</div>
